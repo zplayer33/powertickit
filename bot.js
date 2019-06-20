bf0abb5a229c309c8dfc9de487fe4bbe367c8dd5
zplayer33 <51510077+zplayer33@users.noreply.github.com>
2019-06-20 20:07:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc |DF_Team`,"http://twitch.tv/Death Shop")
+client.user.setGame(`$bc |$help`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
